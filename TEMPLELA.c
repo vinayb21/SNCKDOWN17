795dcc3165e8d7dc63f7ca7d97d903b06013bf37
Ayush Chauhan <ayoushchauhan1996@gmail.com>
2017-05-21 00:51:54 +0530

    Create TEMPLELA.c

---
+++ TEMPLELA.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,l,i,c;
+	scanf("%d",&n);
+	while(n--){
+	    scanf("%d",&l);
+	   	if(l%2==0){
+	   	    printf("no\n");
+	   	}
+	   	else{
+    	   	int a[l]={0};
+    	    for(i=0;i<l;i++){
+    	        scanf("%d",&a[i]);
+    	    }
+    	    if(a[0]==1){
+        	    c=0;
+        	    for(i=0;i<l/2;i++){
+        	        if(i+1==a[i]){
+        	            c++;
+        	        }
+        	    }
+        	    for(i=l/2;i<l;i++){
+        	        if(l-i==a[i]){
+        	            c++;
+        	        }
+        	    }
+        	    if(c==l){
+        	        printf("yes\n");
+        	    }
+        	    else{
+        	        printf("no\n");
+        	    }
+    	    }
+    	    else
+    	        printf("no\n");
+	   	}
+	}
+	return 0;
+}
